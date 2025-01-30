532606efe50c5987b3bb20e951e14a29e31a0b99
liranbarzilay <liranbarzilay@gmail.com>
2025-01-30 13:32:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PIL import Image
 import numpy as np
-from scipy.signal import convolve2d
+from scipy.ndimage import convolve
 from image_utils import load_image, edge_detection
 from skimage.filters import median
 from skimage.morphology import ball
